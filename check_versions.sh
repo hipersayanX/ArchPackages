cab7695a409458e8391d8ace2b915f4b98f337df
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2019-04-15 17:23:31 -0300

    Added script for checking packages versions. Added android-opus and android-libvpx. Removed unnecessary packages.

---
+++ check_versions.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+packages=('android-ffmpeg ffmpeg'
+          'android-libogg libogg'
+          'android-libvorbis libvorbis'
+          'android-libvpx libvpx'
+          'android-opus opus'
+          'android-qt5 qt5-base'
+          'mingw-w64-ffmpeg-zeranoe ffmpeg')
+
+export LC_ALL=C
+
+for package in "${packages[@]}"; do
+    pkg=$(echo $package | awk '{print $1}')
+    pkg_ver=$(grep 'pkgver=' $pkg/PKGBUILD | awk -F'=' '{print $2}')
+    pkg_orig=$(echo $package | awk '{print $2}')
+    pkg_orig_ver=$(pacman -Si $pkg_orig | grep Version | awk '{print $3}' | cut -d "-" -f1 | cut -d ":" -f2)
+    older_ver=$(printf "${pkg_ver}\n${pkg_orig_ver}" | sort -V | head -n 1)
+
+    if [ "${older_ver}" = "${pkg_orig_ver}" ]; then
+        echo -e "$pkg \e[32m$pkg_ver\e[0m"
+    else
+        echo -e "$pkg \e[31m$pkg_ver -> ${pkg_orig_ver}\e[0m"
+    fi
+done
