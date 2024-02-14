ae156dc67ddfb3195fea9e55f94c9be51041c740
Gonzalo Exequiel Pedone <hipersayan.x@gmail.com>
2024-02-14 15:32:11 -0300

    Packages updated: android-environment, android-expat, android-meson, android-pkg-config, weblate-wlc. Packages added: android-cairo-bootstrap, android-fontconfig, android-freetype2-bootstrap, android-glib2, android-harfbuzz, android-icu, android-libffi, android-lzo, android-ncurses, android-pcre2, android-pixman, android-readline.

--- check_versions.sh
+++ check_versions.sh
@@ -2,16 +2,23 @@
 
 packages=('android-brotli brotli'
           'android-bzip2 bzip2'
+          'android-cairo-bootstrap cairo'
           'android-expat expat'
           'android-ffmpeg ffmpeg'
+          'android-fontconfig fontconfig'
+          'android-freetype2-bootstrap freetype2'
           'android-fribidi fribidi'
           'android-gettext gettext'
           'android-giflib giflib'
+          'android-glib2 glib2'
           'android-gmp gmp'
           'android-graphite graphite'
+          'android-harfbuzz harfbuzz'
+          'android-icu icu'
           'android-l-smash l-smash'
           'android-lame lame'
           'android-lcms2 lcms2'
+          'android-libffi libffi'
           'android-libiconv libiconv'
           'android-libjpeg-turbo libjpeg-turbo'
           'android-libogg libogg'
@@ -24,9 +31,14 @@ packages=('android-brotli brotli'
           'android-libvpx libvpx'
           'android-libwebp libwebp'
           'android-libxml2 libxml2'
+          'android-lzo lzo'
+          'android-ncurses ncurses'
           'android-openjpeg2 openjpeg2'
           'android-opus opus'
           'android-pcre pcre'
+          'android-pcre2 pcre2'
+          'android-pixman pixman'
+          'android-readline readline'
           'android-speex speex'
           'android-speexdsp speexdsp'
           'android-termcap termcap'
@@ -41,7 +53,11 @@ export LC_ALL=C
 
 for package in "${packages[@]}"; do
     pkg=$(echo $package | awk '{print $1}')
-    pkg_ver=$(grep 'pkgver=' $pkg/PKGBUILD | awk -F'=' '{print $2}')
+    lines=$(grep -n 'build() {' "${PWD}/${pkg}/PKGBUILD" | awk -F: '{print $1}')
+    lines=$(echo "${lines} - 1" | bc)
+    pkgBuildLines=$(head -n "${lines}" "${PWD}/${pkg}/PKGBUILD")
+    pkgverLine="${pkgBuildLines};echo \${pkgver}"
+    pkg_ver=$(echo "${pkgverLine}" | sh)
     pkg_orig=$(echo $package| awk '{print $2}')
     pkg_info=$(pacman -Si $pkg_orig 2>/dev/null)
 
